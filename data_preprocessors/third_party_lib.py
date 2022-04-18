77a34fc9617e47e44e3783547f806a0d7227007e
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-04-18 17:49:11 +0600

    Add flair and spacy pos tagger

--- data_preprocessors/third_party_lib.py
+++ data_preprocessors/third_party_lib.py
@@ -1,4 +1,7 @@
 import nltk
+import spacy
+from flair.data import Sentence
+from flair.models import SequenceTagger
 from deep_translator import GoogleTranslator
 
 
@@ -16,11 +19,31 @@ def words_to_tags(sentence):
 # ========================================
 # Get Words Tags Dictionary
 # ========================================
-def get_en_word_tag_dict(target=""):
-    tgt_token = nltk.tokenize.word_tokenize(target)
-    en_word_tag_dict = dict((key, value)
-                            for key, value in nltk.pos_tag(tgt_token))
-    return en_word_tag_dict
+def get_nltk_postag_dict(target=""):
+    ''' Get nltk pos tags '''
+    target_tokenized = nltk.tokenize.word_tokenize(target)
+    nltk_postag_dict = dict((key, value)
+                            for key, value in nltk.pos_tag(target_tokenized))
+    return nltk_postag_dict
+
+
+def get_spacy_postag_dict(target=""):
+    ''' Get spacy pos tags '''
+    nlp = spacy.load("en_core_web_sm")
+    target_tokenized = nlp(target)
+    spacy_postag_dict = dict((token.text, token.tag_)
+                             for token in target_tokenized)
+    return spacy_postag_dict
+
+
+def get_flair_postag_dict(target=""):
+    ''' Get flair pos tags '''
+    tagger = SequenceTagger.load("pos")
+    target_tokenized = Sentence(target)
+    tagger.predict(target_tokenized)
+    flair_postag_dict = dict((token.text, token.tag)
+                             for token in target_tokenized)
+    return flair_postag_dict
 
 
 # ========================
