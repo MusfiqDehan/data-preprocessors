4b5c822e8198073a3d99e77e13d0bf2193ea4b5e
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-05-20 05:17:51 +0600

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='data-preprocessors',
+    version='0.26.0',
+    author='Md. Musfiqur Rahaman',
+    author_email='musfiqur.rahaman@northsouth.edu',
+)
