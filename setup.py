551dc54d6f77f3df9f06d1c93bee1324ceea8e22
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2023-10-22 14:37:15 +0600

    Update changelog for v0.40.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='data-preprocessors',
-    version='0.39.0',
+    version='0.40.0',
     author='Md. Musfiqur Rahaman',
     author_email='musfiqur.rahaman@northsouth.edu',
 )
