13c9a32b0d1e91216d5daf7594eebf12a75a7dd7
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2024-07-07 23:46:12 +0600

    Change Version to 0.41.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='data-preprocessors',
-    version='0.40.0',
+    version='0.41.0',
     author='Md. Musfiqur Rahaman',
     author_email='musfiqur.rahaman@northsouth.edu',
 )
