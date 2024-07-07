9ea72b8113580a4456da3561ae4e6f682ab04dff
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2024-07-08 04:54:01 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='data-preprocessors',
-    version='0.46.0',
+    version='0.47.0',
     author='Md. Musfiqur Rahaman',
     author_email='musfiqur.rahaman@northsouth.edu',
 )
