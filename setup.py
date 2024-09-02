a481223ca327e05885793acdf469dbcee6509ba4
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-09-03 04:09:01 +0600

    Update changelog for v0.41.0

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='data-preprocessors',
-    version='0.40.0',
-    author='Md. Musfiqur Rahaman',
-    author_email='musfiqur.rahaman@northsouth.edu',
+    name="data-preprocessors",
+    version="0.41.0",
+    author="Md. Musfiqur Rahaman",
+    author_email="musfiqur.rahaman@northsouth.edu",
 )
